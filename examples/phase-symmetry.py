485a20656578a8f6a14013f26e2b20d90e8125c5
Dženan Zukić <dzenan.zukic@kitware.com>
2025-01-27 15:31:29 -0500

    STYLE: Update the Python example

--- examples/phase-symmetry.py
+++ examples/phase-symmetry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#==========================================================================
+# ==========================================================================
 #
 #   Copyright NumFOCUS
 #
@@ -15,20 +15,26 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#==========================================================================*/
+# ==========================================================================*/
 
 import itk
 import argparse
 import numpy as np
 
-def run(input_image_file, output_image_file,
-        wavelengths=None,
-        sigma=0.55,
-        polarity=0,
-        noise_threshold=10.0):
-    input_image = itk.imread(input_image_file, itk.ctype('float'))
+
+def run(
+    input_image_file,
+    output_image_file,
+    wavelengths=None,
+    sigma=0.55,
+    polarity=0,
+    noise_threshold=10.0,
+):
+    input_image = itk.imread(input_image_file, itk.ctype("float"))
     boundary_condition = itk.PeriodicBoundaryCondition[type(input_image)]()
-    padded = itk.fft_pad_image_filter(input_image, boundary_condition=boundary_condition)
+    padded = itk.fft_pad_image_filter(
+        input_image, boundary_condition=boundary_condition
+    )
 
     dimension = input_image.GetImageDimension()
 
@@ -64,23 +70,36 @@ def run(input_image_file, output_image_file,
     output_image = phase_symmetry_filter.GetOutput()
     itk.imwrite(output_image, output_image_file, True)
 
-def main():
-    parser = argparse.ArgumentParser(description='Compute image phase symmetry.',
-        epilog='Example invocation: phase-symmetry.py --wavelengths 10 10 10 20 20 20 30 30 30 --sigma 0.25 --polarity 1 --noise-threshold 10.0 -- /tmp/HeartUltrasound.mha /tmp/HeartUltrasoundPhaseSymmetry.mha')
 
-    parser.add_argument('input_image')
-    parser.add_argument('output_image')
-    parser.add_argument('-w', '--wavelengths', dest='wavelengths', nargs='+',
-            type=float)
-    parser.add_argument('-s', '--sigma', dest='sigma', type=float, default=0.55)
-    parser.add_argument('-p', '--polarity', dest='polarity', type=int,
-            choices=[-1, 0, 1], default=0)
-    parser.add_argument('-n', '--noise-threshold', dest='noise', type=float,
-            default=10.0)
+def main():
+    parser = argparse.ArgumentParser(
+        description="Compute image phase symmetry.",
+        epilog="Example invocation: phase-symmetry.py --wavelengths 10 10 10 20 20 20 30 30 30 --sigma 0.25 --polarity 1 --noise-threshold 10.0 -- /tmp/HeartUltrasound.mha /tmp/HeartUltrasoundPhaseSymmetry.mha",
+    )
+
+    parser.add_argument("input_image")
+    parser.add_argument("output_image")
+    parser.add_argument(
+        "-w", "--wavelengths", dest="wavelengths", nargs="+", type=float
+    )
+    parser.add_argument("-s", "--sigma", dest="sigma", type=float, default=0.55)
+    parser.add_argument(
+        "-p", "--polarity", dest="polarity", type=int, choices=[-1, 0, 1], default=0
+    )
+    parser.add_argument(
+        "-n", "--noise-threshold", dest="noise", type=float, default=10.0
+    )
     args = parser.parse_args()
 
-    run(args.input_image, args.output_image, args.wavelengths, args.sigma,
-            args.polarity, args.noise)
+    run(
+        args.input_image,
+        args.output_image,
+        args.wavelengths,
+        args.sigma,
+        args.polarity,
+        args.noise,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
